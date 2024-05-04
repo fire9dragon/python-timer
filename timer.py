6ceffdd468def80f14f6c261258e8bc6117025d1
fire9dragon <168269207+fire9dragon@users.noreply.github.com>
2024-05-04 10:39:26 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,12 +1,17 @@
 from time import sleep
 import tkinter as tk
-from subprocess import call as run
+import os
+os_name = os.name
+if os_name == "posix":
+    command = "clear"
+else:
+    command = "cls"
 
 timer_time = int(input("Enter the how long the timer shoud be in Seconds: "))
 for i in range(timer_time):
     print(f'{"Seconds passed: "} {i} {"/"} {timer_time}')
     sleep(1)
-    run("clear")
+    os.system(command)
 
 labal = tk.Label(None, text="Times up!!",font=('Times', '18'),fg='red')
 labal.pack()
